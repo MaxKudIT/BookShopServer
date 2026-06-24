72cf95013c0df766741250ec376122d8f5cb958d
MaxKudIT <kudinovmaks143@gmail.com>
2026-06-24 10:28:13 +0700

    корректировка разрешения запросов

--- internal/transport/web/server/server.go
+++ internal/transport/web/server/server.go
@@ -11,8 +11,8 @@ func (s *server) Create() *gin.Engine {
 	router := gin.Default()
 
 	router.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:5173"},
-		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowOrigins:     []string{"http://localhost:5173", "http://127.0.0.1:5173", "http://192.168.0.109:5173"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
 		AllowCredentials: true,
